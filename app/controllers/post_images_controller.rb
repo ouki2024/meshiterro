38d865e23f68aa685fddf01454211dcecc42872c
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-03-18 14:08:42 +0000

    [途中]画像投稿

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,7 +8,7 @@ class PostImagesController < ApplicationController
     # 投稿データの保存
     def create
         @post_image = PostImage.new(post_image_params)
-        @post_imaeg.user_id = current_user.id
+        @post_image.user_id = current_user.id
         @post_image.save
         redirect_to post_images_path
     end
