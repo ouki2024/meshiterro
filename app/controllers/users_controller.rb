fc50f9744cd17f915ed0cf3d610fae49e0b78c84
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2024-03-28 10:28:37 +0000

    [finish]24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
   
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
     
+    @user = User.find(params[:id])
   end
   
   def update
+  
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to user_path(@user.id)
   end
-
 
   private
 
@@ -23,5 +25,14 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
   
+   # ここから追加
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
   
 end
+
